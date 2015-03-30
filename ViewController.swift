a6686641e6c5ed1fce4623fe3eaf762f6ff234b7
Thomas Brittain <design@ubermentis.com>
2015-03-29 22:35:38 -0500

    Setup selecting data based on row.

--- ViewController.swift
+++ ViewController.swift
@@ -8,32 +8,120 @@
 
 import UIKit
 
+//  Global variables for SAL data.
+var serviceCodes = Array<String>()
+var serviceDescription = Array<String>()
+var whenToUseCode = Array<String>()
+
+// Used to key dictionary to rows.
+var serviceCodesCount: Int = 0
+var serviceCodeLookup: [String: Int] = ["" : 0]
+
+//  Setup initial CSV data.
+var columns = Dictionary<String, String>()
+var rows = Dictionary<String, String>()
+var headers: String = ""
+
+// Used to pass selected row to detailView
+var selectionValue: Int = 0
+
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet var tableView: UITableView!
-
-    var items: [String] = ["We", "Heart", "Swift"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Setup a blank cell.
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        initSALFile()
     }
     
-    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items.count;
+        return serviceCodesCount
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
         
-        cell.textLabel?.text = self.items[indexPath.row]
-        
+        // Load cell; set labels to CSV data.
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = serviceCodes[indexPath.row]
+        println(serviceDescription[indexPath.row])
+        cell.detailTextLabel?.text = serviceDescription[indexPath.row]
+
         return cell
     }
     
+    
+    
     func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
-        println("You selected cell #\(indexPath.row)!")
+        
+        // Gets int for selected row.
+        let selection = indexPath.row
+        selectionValue = indexPath.row
+        
+        // This gets the path to a local resource and unwraps it.
+        if let path = NSBundle.mainBundle().pathForResource("SAL_CODEs_v1", ofType: "csv"){
+            if let url = NSURL(fileURLWithPath: path){
+                var error: NSErrorPointer = nil
+                if let csv = CSV(contentsOfURL: url, error: error) {
+                    
+                    // Rows
+                    let rows = csv.rows[selection]
+                }
+            }
+        }
+        
+        // Load the UIViewController, Nib, then push to it.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewControllerWithIdentifier("detailViewController") as UIViewController
+        navigationController?.pushViewController(vc, animated: true)
+        
     }
-}
 
+
+// MARK: SAL
+
+    func initSALFile(){
+    
+        // This gets the path to a local resource and unwraps it.
+        if let path = NSBundle.mainBundle().pathForResource("SAL_CODEs_v1", ofType: "csv"){
+            
+            //let url = NSURL(string: path)
+            if let url = NSURL(fileURLWithPath: path){
+                var error: NSErrorPointer = nil
+                if let csv = CSV(contentsOfURL: url, error: error) {
+
+                    // Rows
+                    var rows = csv.rows
+                    let headers = csv.headers  //=> ["id", "name", "age"]
+                    // Columns
+                    let columns = csv.columns
+                    serviceCodes = columns["SER"]!
+                    serviceDescription = columns["SERVICE DESCRIPTION & DEFINITIONS"]!
+                    whenToUseCode = columns["WHEN TO USE THIS CODE (EXAMPLES)"]!
+                    
+                    // Build a service-code lookup.
+                    if let serviceCodes = csv.columns["SER"]{
+                        for (index, element) in enumerate(serviceCodes) {
+                            serviceCodeLookup[element] = (index)
+                        }
+                    }
+                    
+                    serviceCodesCount = serviceCodes.count
+
+                    // Create a row buffer for the row we are going to look-up based on
+                    // the service code.
+                    let rowBfr: Array = csv.columns["SER"]!
+                    if let serviceCodeRow = serviceCodeLookup["33"]{
+                        let serviceCodeRow = serviceCodeLookup["33"]
+                    }
+                } // Unwrap CSV.
+            } // Unwrap URL
+        } // Unwrap PATH
+    }
+    
+    
+    
+    
+}
--- detailViewController.swift
+++ detailViewController.swift
@@ -9,8 +9,12 @@
 import UIKit
 
 class detailViewController: UIViewController {
+    @IBOutlet weak var activityName: UILabel!
+    @IBOutlet weak var serviceDescriptionTextView: UITextView!
+
     
     override func viewDidLoad() {
+
         
     }
     
@@ -21,9 +25,18 @@ class detailViewController: UIViewController {
         navigationItem.leftBarButtonItem = saveButton
         //navigationController!.navigationBar.barTintColor = UIColor.greenColor()
         //title = "Activity Details"
+        
+        self.activityName.text = "Waahoo!"
+        self.serviceDescriptionTextView.text = serviceDescription[selectionValue]
+        
     }
     
+    
     func saveButton() {
+        // Load the UIViewController, Nib, then push to it.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewControllerWithIdentifier("mainViewController") as UIViewController
+        navigationController?.popToRootViewControllerAnimated(true)
         println("Yo")
     }
 }
